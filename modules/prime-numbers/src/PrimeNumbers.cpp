7e272655bb7d65f9f30ba57ccffcbd2f7dea3600
HAXAJI2012 <haxaji2012@gmail.com>
2018-05-05 21:03:21 +0300

    Added one more space

--- modules/prime-numbers/src/PrimeNumbers.cpp
+++ modules/prime-numbers/src/PrimeNumbers.cpp
@@ -20,7 +20,7 @@ std::vector<int> primeNumbers::findPrimeNums(int min, int max) {
     for (int i = 0; i < size; i++) {
         for (int j = 2; j <= sqrt((double)arr[i]); j++)
             if (arr[i] % j == 0 && arr[i] != j && arr[i] != 1) {
-                arr[i] = 0; // 0 - not a prime number
+                arr[i] = 0;  // 0 - not a prime number
                 break;
             }
     }
